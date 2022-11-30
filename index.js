f38f0c8233361801dc4cf52f53e50941e1515e02
Diego <extraniastravesiasdejojo@gmail.com>
2022-11-29 22:33:53 -0500

    changes to crud + fixes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-let base = "localhost:8080";
+let baseUrl = "http://localhost:8080";
 let usuarios = [];
-let articulos = [];
-let imagenes = [];
-
+// Funciones para HeaderFooter Universales
 function ImprimirHeaderFooter(){
     let contenedor = document.getElementById("top");
     contenedor.innerHTML = ObtenerHeader();
@@ -45,4 +43,89 @@ function ObtenerFooter(){
         </ul>   
     </section>
     </footer>`;
+    }
+   
+// Funciones para CRUD y etc
+const targets = document.querySelectorAll('[data-target]');
+const content = document.querySelectorAll('[data-content]');
+targets.forEach(target => {
+	target.addEventListener('click', () => {
+		content.forEach(c => {
+			c.classList.remove('active')
+		})
+		const t = document.querySelector(target.dataset.target)
+		t.classList.add('active')
+	})
+})
+
+function ObtenerUsuarios() {
+  fetch(baseUrl + '/rushevo_db/usuarios/all').then(res => {
+    res.json().then(json => {
+      usuarios = json;
+      console.log(usuarios);
+      console.log('1');
+      ImprimirUsuario();
+    });
+  });
 }
+
+
+function ImprimirUsuario() {
+    let contenedor = document.getElementById("cuerpoTablaUsuarios");
+  
+    usuarios.forEach(usuario => {
+      contenedor.innerHTML += MapearUsuario(usuario);
+    });
+  }
+
+  function MapearUsuario(usuario) {
+    return `<tr>
+    <td>
+      <button class='btn btn-danger btn-sm' onclick="EliminarProducto(${usuario.id_user})">Eliminar</button>
+      <button class='btn btn-warning btn-sm' onclick="PopularDatosCampos(${usuario.id_user})">Actualizar</button>
+      </td>
+    <td>${usuario.id_user}</td>
+    <td>${usuario.email}</td>
+    <td>${usuario.nombre}</td>
+    <td>${usuario.apellido}</td>
+    <td>${usuario.fecha_nac}</td>
+    
+  </tr>`;
+  }
+
+  function EliminarUsuario(pid) {
+    fetch(baseUrl + '/usuario/' + pid, { method: "Delete" }).then(res => {
+      console.log(res);
+      ObtenerUsuarios();
+    });
+  }
+
+  function GuardarUsuario() {
+    let data = {
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+      nombre: document.getElementById("nombre").value,
+      apellido: document.getElementById("apellido").value,
+      fecha_nac: document.getElementById("fecha_nac").value,
+    };
+  
+    fetch(baseUrl + "/rushevo_db/usuarios/", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": 'application/json; charset=UTF-8'
+      }
+    }).then(res => {
+        ObtenerUsuarios();
+    });
+  }
+
+  function PopularDatosCampos(pid) {
+    let usuario = usuarios.filter(p => { return p.id == pid })[0];
+  
+    document.getElementById('id_user').value = usuario.id_user;
+    document.getElementById('email').value = usuario.email;
+    document.getElementById('nombre').value = usuario.nombre;
+    document.getElementById('apellido').value = usuario.apellido;
+    document.getElementById('fecha_nac').value = usuario.fecha_nac;
+  }
